38e89f51a4d04970787ca5e3fab6d6fe89fba832
Kyle Fuller <kyle@fuller.li>
2015-10-12 18:09:10 -0700

    [Version] Support prereleases

--- Conche/Version.swift
+++ Conche/Version.swift
@@ -1,16 +1,32 @@
+extension String {
+  func split(element: Character, maxSplit: Int = Int.max) -> [String] {
+    return characters.split(maxSplit, isSeparator: { $0 == element }).map(String.init)
+  }
+}
+
+/// Represents a semantic version
 public struct Version : CustomStringConvertible, Equatable, Comparable {
   public let major: Int
   public let minor: Int
   public let patch: Int
+  public let prerelease: String?
 
-  public init(major: Int, minor: Int, patch: Int) {
+  public init(major: Int, minor: Int, patch: Int, prerelease: String? = nil) {
     self.major = major
     self.minor = minor
     self.patch = patch
+    self.prerelease = prerelease
   }
 
   public init(_ value: String) throws {
-    let components = value.characters.split { $0 == "." }.map(String.init)
+    let prereleaseComponents = value.split("-", maxSplit: 2)
+    if prereleaseComponents.count == 2 {
+      prerelease = prereleaseComponents.last
+    } else {
+      prerelease = nil
+    }
+
+    let components = prereleaseComponents[0].split(".")
     if components.count == 3, let major = Int(components[0]), minor = Int(components[1]), patch = Int(components[2]) {
       self.major = major
       self.minor = minor
@@ -21,15 +37,23 @@ public struct Version : CustomStringConvertible, Equatable, Comparable {
   }
 
   public var description:String {
+    if let prerelease = prerelease {
+      return "\(major).\(minor).\(patch)-\(prerelease)"
+    }
+
     return "\(major).\(minor).\(patch)"
   }
 }
 
 public func == (lhs:Version, rhs:Version) -> Bool {
-  return lhs.major == rhs.major && lhs.minor == rhs.minor && lhs.patch == rhs.patch
+  return lhs.major == rhs.major &&
+    lhs.minor == rhs.minor &&
+    lhs.patch == rhs.patch &&
+    lhs.prerelease == rhs.prerelease
 }
 
 public func < (lhs:Version, rhs:Version) -> Bool {
+  // TODO prerelease
   if lhs.major == rhs.major {
     if lhs.minor == rhs.minor {
       return lhs.patch < rhs.patch
--- ConcheTests/VersionSpec.swift
+++ ConcheTests/VersionSpec.swift
@@ -3,32 +3,37 @@ import Conche
 
 
 describe("Version") {
-  let version = Version(major: 1, minor: 0, patch: 0)
+  let version = Version(major: 1, minor: 2, patch: 3, prerelease: "beta.1")
 
   $0.it("has a major") {
     try equal(version.major, 1)
   }
 
   $0.it("has a minor") {
-    try equal(version.minor, 0)
+    try equal(version.minor, 2)
   }
 
   $0.it("has a patch") {
-    try equal(version.patch, 0)
+    try equal(version.patch, 3)
+  }
+
+  $0.it("has a prerelease") {
+    try equal(version.prerelease, "beta.1")
   }
 
   $0.it("can be converted to a string") {
-    try equal(version.description, "1.0.0")
+    try equal(version.description, "1.2.3-beta.1")
   }
 
-  $0.it("is equal to version with the same major, minor and patch") {
-    try equal(version, Version(major: 1, minor: 0, patch: 0))
+  $0.it("is equal to version with the same major, minor, patch and pre-release") {
+    try equal(version, Version(major: 1, minor: 2, patch: 3, prerelease: "beta.1"))
   }
 
-  $0.it("is not equal to version with a different major, minor and patch") {
-    try notEqual(version, Version(major: 2, minor: 0, patch: 0))
-    try notEqual(version, Version(major: 1, minor: 1, patch: 0))
-    try notEqual(version, Version(major: 1, minor: 0, patch: 1))
+  $0.it("is not equal to version with a different major, minor, patch or pre-release") {
+    try notEqual(version, Version(major: 0, minor: 2, patch: 3, prerelease: "beta.1"))
+    try notEqual(version, Version(major: 1, minor: 0, patch: 3, prerelease: "beta.1"))
+    try notEqual(version, Version(major: 1, minor: 2, patch: 0, prerelease: "beta.1"))
+    try notEqual(version, Version(major: 1, minor: 2, patch: 3, prerelease: "beta.2"))
   }
 
   $0.describe("when parsing a string") {
@@ -38,6 +43,16 @@ describe("Version") {
       try equal(version.major, 1)
       try equal(version.minor, 2)
       try equal(version.patch, 3)
+      try `nil`(version.prerelease)
+    }
+
+    $0.it("parses the major, minor, patch with a pre-release component") {
+      let version = try Version("1.2.3-beta.1")
+
+      try equal(version.major, 1)
+      try equal(version.minor, 2)
+      try equal(version.patch, 3)
+      try equal(version.prerelease, "beta.1")
     }
   }
 }
